72f82e1b37929eccf0f6398eb9556d1355127bbf
mabismaramadhan <mabismaramadhan@gmail.com>
2023-05-01 16:46:36 +0700

    Update BahanMakanan.java

--- sourcecode/BahanMakanan.java
+++ sourcecode/BahanMakanan.java
@@ -1,14 +1,48 @@
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class BahanMakanan extends Objek {
+    public String[] bahanMakanan;
     private static String[] namaBahanMakanan = {
-            "Nasi", "Kentang", "Ayam", "Sapi", "Wortel", "Bayam", "Kacang", "Susu"
+            "Nasi", 
+            "Kentang",
+            "Ayam",
+            "Sapi",
+            "Wortel",
+            "Bayam",
+            "Kacang",
+            "Susu"
     };
     private static int[] hargas = {
-            5, 3, 10, 12, 3, 3, 2, 2
+            5,
+            3,
+            10,
+            12,
+            3,
+            3,
+            2,
+            2
     };
     private static int[] kekenyangans = {
-            5, 4, 8, 15, 2, 2, 2, 1
+            5,
+            4,
+            8,
+            15,
+            2,
+            2,
+            2,
+            1
+    };
+
+    private static int[] kuantitasInventory =
+    {
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0
     };
 
     private int harga;
@@ -20,8 +54,43 @@ public class BahanMakanan extends Objek {
         kekenyangan = kekenyangans[index];
     }
 
-    public String getBahanMakanan() {
-        return getNama();
+    private int namaBahanMakanan(String bahanMakanan)
+    {
+        String[] daftarNamaBahanMakanan = {"Nasi", "Kentang", "Ayam", "Sapi", "Wortel", "Bayam", "Kacang", "Susu"};
+        for (int i = 0; i < daftarNamaBahanMakanan.length; i++){
+            if (daftarNamaBahanMakanan[i].equals(bahanMakanan)){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public void tambahBahanMakanan (String bahanMakanan)
+    {
+        int indeksBahanMakanan = namaBahanMakanan(bahanMakanan);
+        kuantitasInventory[indeksBahanMakanan] += 1;
+    }
+
+    public void hapusBahanMakanan(String bahanMakanan)
+    {
+        int indeksBahanMakanan = namaBahanMakanan(bahanMakanan);
+        kuantitasInventory[indeksBahanMakanan] -= 1;
+    }
+
+    public void printBahanMakanan()
+    {
+        System.out.println("Daftar Bahan Makanan yang dimiliki:");
+        for (int i = 0; i < kuantitasInventory.length; i++){
+            if (kuantitasInventory[i] > 0)
+            {
+                System.out.println(namaBahanMakanan[i] + ": " + kuantitasInventory[i]);
+            }
+        }
+    }
+
+    public String getBahanMakanan(int index) {
+        return namaBahanMakanan[index];
     }
 
     public int getHarga() {
@@ -32,23 +101,23 @@ public class BahanMakanan extends Objek {
         return kekenyangan;
     }
 
-    public static ArrayList<BahanMakanan> inisialisasiBahanMakanan() {
-        ArrayList<BahanMakanan> listBahanMakanan = new ArrayList<>();
-        for (int i = 0; i < namaBahanMakanan.length; i++) {
-            listBahanMakanan.add(new BahanMakanan(i));
-        }
-        return listBahanMakanan;
-    }
+//    public static ArrayList<BahanMakanan> inisialisasiBahanMakanan() {
+//        ArrayList<BahanMakanan> listBahanMakanan = new ArrayList<>();
+//        for (int i = 0; i < namaBahanMakanan.length; i++) {
+//            listBahanMakanan.add(new BahanMakanan(i));
+//        }
+//        return listBahanMakanan;
+//    }
 
-    public static void tampilkanBahanMakanan(ArrayList<BahanMakanan> listBahanMakanan) {
-        System.out.println("Daftar Bahan Makanan yang dimiliki:");
-        for (BahanMakanan bahanMakanan : listBahanMakanan) {
-            System.out.println("Nama Bahan Makanan: " + bahanMakanan.getBahanMakanan() + ", Harga: " + bahanMakanan.getHarga() + ", Kekenyangan: " + bahanMakanan.getKekenyangan());
-        }
-    }
+//    public static void tampilkanBahanMakanan(ArrayList<BahanMakanan> listBahanMakanan) {
+//        System.out.println("Daftar Bahan Makanan yang dimiliki:");
+//        for (BahanMakanan bahanMakanan : listBahanMakanan) {
+//            System.out.println("Nama Bahan Makanan: " + bahanMakanan.getBahanMakanan() + ", Harga: " + bahanMakanan.getHarga() + ", Kekenyangan: " + bahanMakanan.getKekenyangan());
+//        }
+//    }
 
-    public static void main(String[] args) {
-        ArrayList<BahanMakanan> listBahanMakanan = inisialisasiBahanMakanan();
-        tampilkanBahanMakanan(listBahanMakanan);
-    }
+ //   public static void main(String[] args) {
+ //       ArrayList<BahanMakanan> listBahanMakanan = inisialisasiBahanMakanan();
+ //       tampilkanBahanMakanan(listBahanMakanan);
+//    }
 }
